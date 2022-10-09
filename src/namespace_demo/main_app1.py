23948afaea92379194cf70c07ed0c479c540edb2
Sury Soni <suryasoni@gmail.com>
2022-10-09 23:53:23 +1100

    add app1 as sub-command

--- src/namespace_demo/main_app1.py
+++ src/namespace_demo/main_app1.py
@@ -1,9 +1,16 @@
+from namespace_demo.main import cli
+
 from namespace_demo.base.base_module import random_name
 from namespace_demo.base.shared import hello_world
 from namespace_demo.app1.upper_case import upper_case
 
 
-def main():
+@cli.command
+def app1():
     name = random_name()
     lower_name = upper_case(name)
     hello_world(lower_name)
+
+
+if __name__ == '__main__':
+    cli()
